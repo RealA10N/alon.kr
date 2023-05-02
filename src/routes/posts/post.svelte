e14964e082619a23c8d67775193055dabf280587
Alon Krymgand <me@alon.kr>
2023-05-02 14:17:24 +0300

    Added mdsvex

---
+++ src/routes/posts/post.svelte
@@ -0,0 +1,16 @@
+<script>
+	/** @type {string} */
+	export let title;
+
+	/** @type {string | undefined} */
+	export let subtitle;
+</script>
+
+<h1>
+	{title}
+	{#if subtitle}
+		<div class="font-light text-base">{subtitle}</div>
+	{/if}
+</h1>
+
+<slot />
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,20 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.md', '.svx'],
+
+	preprocess: [
+		vitePreprocess(),
+		mdsvex({
+			extensions: ['.md', '.svx'],
+			layout: 'src/routes/posts/post.svelte'
+		})
+	],
 
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
