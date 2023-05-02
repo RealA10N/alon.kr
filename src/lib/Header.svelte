5e2acef50e981e7dd70ef324107119d81bb866a9
Alon Krymgand <me@alon.kr>
2023-05-02 20:43:35 +0300

    Added red underline in header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -2,6 +2,14 @@
 	import '@fontsource/zilla-slab/300-italic.css';
 </script>
 
-<div class="text-center font-thin italic my-8">
-	<a href="/"> Alon Krymgand </a>
+<div class="text-center font-thin italic my-8 bg-zinc-100 dark:bg-zinc-900 z-50">
+	<a
+		href="/"
+		class="relative before:absolute before:left-0 before:bottom-0
+			before:w-full before:h-[2px] before:rounded no-underline
+			before:bg-red-500 before:transition-transform
+			before:scale-x-100 before:hover:scale-x-105"
+	>
+		Alon Krymgand
+	</a>
 </div>
