8c427f3afbe8b7300004f9e17798edcccc6a7d39
Alon Krymgand <me@alon.kr>
2023-05-06 12:52:42 +0300

    Post URLs now generated automatically

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,9 +4,14 @@ import type { PageServerLoad } from './$types';
 
 export const prerender = true;
 
+function globToUrl(glob: string) {
+    const sections = glob.split('/');
+    return [''].concat(sections.splice(1, sections.length-2)).join('/');
+}
+
 export const load = (async () => {
     const globs = import.meta.glob('./posts/*/+page.md', { as: 'raw' });
-    const contents = await Promise.all(Object.values(globs).map(fn => fn()));
-    const posts = contents.map(raw => matter(raw).data as Post);
+    const contents = await Promise.all(Object.entries(globs).map(async ([glob, fn]) => [globToUrl(glob), await fn()]));
+    const posts = contents.map(([url, content]) => ({...matter(content).data, url: url} as Post));
     return { posts: posts };
 }) satisfies PageServerLoad;
