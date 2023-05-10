da262a61e27a967803f425e87d725f20b3ccd815
Alon Krymgand <me@alon.kr>
2023-05-10 09:04:06 +0300

    Added references and footnote components

---
+++ src/lib/Footnote.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let title: string;
+	export let url: string;
+	export let people: string | string[];
+	export let references: { title: string; url: string; people: string | string[] }[];
+
+	references.push({ title: title, url: url, people: people });
+	export let number = references.length;
+</script>
+
+<sup id="src{number}"><a href="#ref{number}"> {number} </a></sup>
---
+++ src/lib/References.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let references: { title: string; url: string; people: string[] | string }[];
+</script>
+
+<ol>
+	{#each references as ref, i}
+		<li id="ref{i + 1}">
+			<a href={ref.url} class="no-underline hover:underline">
+				{ref.title}
+				<span class="italic text-sm">
+					{#if typeof ref.people === 'string'}
+						{ref.people}
+					{:else}
+						{ref.people?.join(', ') ?? ''}
+					{/if}
+				</span>
+			</a>
+			<a href="#src{i + 1}" class="no-underline text-sm">↩</a>
+		</li>
+	{/each}
+</ol>
