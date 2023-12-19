00c8b94d7b813b3b4d6fca11a8d25ddf0053900d
Alon Krymgand <me@alon.kr>
2023-12-19 19:23:35 +0200

    Added the RandomTransform effect component

---
+++ src/lib/effects/RandomTransform.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let x: [number, number], y: [number, number];
+	const random = (a: number, b: number) => Math.random() * (b - a + 1) + a;
+	$: dx = random(x[0], x[1]);
+	$: dy = random(y[0], y[1]);
+</script>
+
+<div style="transform: translate({dx}px, {dy}px)">
+	<slot />
+</div>
