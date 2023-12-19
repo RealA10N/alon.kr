4e3763e7f3703efa7b8242a164434bbf25ae214b
Alon Krymgand <me@alon.kr>
2023-12-19 02:00:19 +0200

    Added permutation component

---
+++ src/lib/permutation/Permutations.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { flip } from 'svelte/animate';
+	import { blur } from 'svelte/transition';
+	import { LetterMode } from '$lib/interfaces/stringMatching';
+	import LetterBox from '$lib/strings/LetterBox.svelte';
+	import type { Permutation } from './permutation';
+	export let permutation: Permutation;
+</script>
+
+<div class="flex flex-wrap justify-center gap-2">
+	{#each permutation as num (num)}
+		<div animate:flip={{ duration: 400 }} in:blur={{ duration: 400 }} out:blur={{ duration: 150 }}>
+			<LetterBox state={{ text: (num + 1n).toString(), mode: LetterMode.Regular }} />
+		</div>
+	{/each}
+</div>
