12098c00a3fd025464b36283093cfcc0cbf07da3
Alon Krymgand <me@alon.kr>
2023-12-20 14:37:46 +0200

    Fixed figure caption & buttons padding

--- src/lib/Figure.svelte
+++ src/lib/Figure.svelte
@@ -2,12 +2,12 @@
 	<slot name="content" />
 
 	{#if $$slots.buttons}
-		<div class="mt-4 sm:px-12 px-2 flex items-baseline justify-center flex-wrap">
+		<div class="mt-4 sm:px-20 px-4 flex items-baseline justify-center flex-wrap">
 			<slot name="buttons" />
 		</div>
 	{/if}
 
-	<figcaption class="text-sm my-2 italic px-12 mx-auto text-zinc-500">
+	<figcaption class="text-sm my-2 italic sm:px-12 px-2 mx-auto text-zinc-500">
 		<slot name="caption" />
 	</figcaption>
 </figure>
