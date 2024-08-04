edbb34f90aaaa201a1d01ad2a853bff1eda6f94e
Alon Krymgand <me@alon.kr>
2024-08-04 23:42:49 +0300

    Added package layout

--- src/lib/interfaces/package.ts
+++ src/lib/interfaces/package.ts
@@ -1,5 +1,15 @@
 export interface Package {
 	name: string;
+	title: string;
 	description: string;
+	versionSourceControl: 'bzr' | 'fossil' | 'git' | 'hg' | 'svn';
 	sourceUrl: string;
 }
+
+export const packageUrl = (pkg: Package) => {
+	return 'alon.kr/x/' + pkg.name;
+};
+
+export const documentationUrl = (pkg: Package) => {
+	return 'https://pkg.go.dev/' + packageUrl(pkg);
+};
--- src/routes/x/PackageLayout.svelte
+++ src/routes/x/PackageLayout.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
-	import type { Package } from '$src/lib/interfaces/package';
+	import { type Package, packageUrl, documentationUrl } from '$src/lib/interfaces/package';
+	import Header from '$src/lib/Header.svelte';
+	import Metadata from '$src/lib/Metadata.svelte';
 
 	export let pkg: Package;
 	export const prerender = true;
 </script>
 
-<h1>{pkg.name}</h1>
+<svelte:head>
+	<meta name="go-import" content="{packageUrl(pkg)} {pkg.versionSourceControl} {pkg.sourceUrl}" />
+</svelte:head>
+
+<Metadata title={pkg.title} description={pkg.description} />
+
+<Header title={pkg.title} description={pkg.description} />
+
+<h2>Get</h2>
+<code>go get {packageUrl(pkg)}</code>
+
+<h2>Source</h2>
+<p>can be found <a href={pkg.sourceUrl}>here</a>.</p>
+
+<h2>Documentation</h2>
+<p>can be found <a href={documentationUrl(pkg)}>here</a>.</p>
 
 <slot />
