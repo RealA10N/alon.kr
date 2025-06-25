09dd7f50ce7f425db0f5ac86b8660c1792bab7e8
Alon Krymgand <me@alon.kr>
2025-06-25 04:36:50 +0300

    `<Th2Circuit />` now uses generic `<Circuit />` component

--- src/routes/posts/circuit-complexity/lib/Th2Circuit.svelte
+++ src/routes/posts/circuit-complexity/lib/Th2Circuit.svelte
@@ -1,92 +1,31 @@
 <script lang="ts">
-	import Graph from '$lib/graphs/Graph.svelte';
 	import Figure from '$lib/Figure.svelte';
-	import FullWidth from '$lib/FullWidth.svelte';
-	import { GraphMode, type Edge, type Vertex } from '$lib/graphs/Graph.svelte';
 	import { Color } from '$lib/interfaces/color';
-	import { onMount } from 'svelte';
-	import BooleanButton from '$lib/logic/BooleanButton.svelte';
-	import BooleanTag from '$lib/logic/BooleanTag.svelte';
-	import AnimationButton from '$lib/AnimationButton.svelte';
-
-	export const prerender = true;
-
-	type GateFunction = (inputs: boolean[]) => boolean;
-
-	interface Gate extends Vertex {
-		compute: GateFunction;
-	}
+	import Circuit, {
+		type Gate,
+		NewAndGate,
+		NewInputGate,
+		NewNotGate,
+		NewOrGate,
+		NewOutputGate
+	} from '$lib/graphs/Circuit.svelte';
+	import { type Edge } from '$lib/graphs/Graph.svelte';
 
 	let inputs: boolean[] = [false, false, false];
-
-	const And = (inputs: boolean[]): boolean => inputs.every(Boolean);
-	const Or = (inputs: boolean[]): boolean => inputs.some(Boolean);
-	const Not = (inputs: boolean[]): boolean => !inputs[0];
-	const Id = (inputs: boolean[]): boolean => inputs[0];
-
-	const NthInput =
-		(n: number) =>
-		(_: boolean[]): boolean =>
-			inputs[n];
+	const getInputs = () => inputs;
 
 	let vertices: Gate[] = [
-		{ id: 1, label: 'x₁', compute: NthInput(0), x: -92, y: -127 },
-		{ id: 2, label: 'x₂', compute: NthInput(1), x: -57, y: -65 },
-		{ id: 3, label: 'x₃', compute: NthInput(2), x: -38, y: 151 },
-		{ id: 4, label: '∧', compute: And, x: 40, y: -136 },
-		{ id: 5, label: '∨', compute: Or, x: -165, y: -16 },
-		{ id: 6, label: '¬', compute: Not, x: 16, y: -14 },
-		{ id: 7, label: '∧', compute: And, x: -69, y: 75 },
-		{ id: 8, label: '∧', compute: And, x: 64, y: 87 },
-		{ id: 9, label: '∨', compute: Or, x: 125, y: -32 },
-		{ id: 10, label: 'y₁', compute: Id, x: 176, y: 79 }
+		{ id: 1, x: -92, y: -127, ...NewInputGate(getInputs, 0) },
+		{ id: 2, x: -57, y: -65, ...NewInputGate(getInputs, 1) },
+		{ id: 3, x: -38, y: 151, ...NewInputGate(getInputs, 2) },
+		{ id: 4, x: 40, y: -136, ...NewAndGate() },
+		{ id: 5, x: -165, y: -16, ...NewOrGate() },
+		{ id: 6, x: 16, y: -14, ...NewNotGate() },
+		{ id: 7, x: -69, y: 75, ...NewAndGate() },
+		{ id: 8, x: 64, y: 87, ...NewAndGate() },
+		{ id: 9, x: 125, y: -32, ...NewOrGate() },
+		{ id: 10, x: 176, y: 79, ...NewOutputGate(0) }
 	].map((v) => ({ ...v, color: Color.Red } as Gate));
-
-	const getForwardEdges = (g: Gate): Edge[] => {
-		return edges.filter((e) => e.source === g);
-	};
-
-	const getBackwardEdges = (g: Gate): Edge[] => {
-		return edges.filter((e) => e.target === g);
-	};
-
-	const getBackwardGates = (g: Gate): Gate[] => {
-		return getBackwardEdges(g).map((e) => e.source) as Gate[];
-	};
-
-	const computeGate = (g: Gate, gateCaches: Map<Gate, boolean>) => {
-		const cachedGates = gateCaches.get(g);
-		if (cachedGates !== undefined) {
-			return cachedGates;
-		}
-
-		const inputGates = getBackwardGates(g);
-		const inputs = inputGates.map((g) => computeGate(g, gateCaches));
-
-		const result = g.compute(inputs);
-		gateCaches.set(g, result);
-		return result;
-	};
-
-	let refresh = () => {};
-
-	const computeFunction = (): boolean[] => {
-		let cachedGates = new Map<Gate, boolean>();
-		for (const gate of vertices) {
-			const state = computeGate(gate, cachedGates);
-			setGateState(gate, state);
-		}
-
-		refresh();
-		return vertices.map((g) => computeGate(g, cachedGates));
-	};
-
-	const setGateState = (gate: Gate, state: boolean) => {
-		gate.color = state ? Color.Red : undefined;
-		for (const edge of getForwardEdges(gate)) {
-			edge.color = state ? Color.Red : undefined;
-		}
-	};
 
 	let edges: Edge[] = [
 		{ source: 1, target: 4 },
@@ -102,99 +41,10 @@
 		{ source: 8, target: 9 },
 		{ source: 9, target: 10 }
 	].map((e) => ({ ...e, direction: true } as Edge));
-
-	let gateResults = computeFunction();
-	$: inputs, vertices, edges, (gateResults = computeFunction());
-
-	const outputIndices: number[] = [9];
-	$: outputs = outputIndices.map((i) => gateResults[i]);
-
-	const inputsToNumber = (inputs: boolean[]): number => {
-		return inputs.reduce((acc, input, i) => acc + (input ? 1 << i : 0), 0);
-	};
-
-	const numberToInputs = (number: number): boolean[] => {
-		return Array.from({ length: inputs.length }, (_, i) => Boolean(number & (1 << i)));
-	};
-
-	const next = () => {
-		let n = inputsToNumber(inputs);
-		n += 1;
-		n %= 1 << inputs.length;
-		inputs = numberToInputs(n);
-	};
-
-	let stop = () => {};
-
-	onMount(computeFunction);
-
-	let width: number;
-	$: responsiveWidth = Math.min(width - 280, 460);
-	$: graphWidth = responsiveWidth > 200 ? responsiveWidth : width;
 </script>
 
 <Figure>
-	<FullWidth slot="content" bind:width>
-		<div class="flex flex-wrap items-center justify-center">
-			<Graph
-				class="inline-block flex-shrink-0"
-				width={graphWidth}
-				{edges}
-				{vertices}
-				vertexLabels={true}
-				mode={GraphMode.sticky}
-				bind:refresh
-			/>
-
-			<div class="flex flex-col items-center justify-center">
-				<table class="m-2 inline-block table-fixed">
-					<colgroup>
-						<col class="w-20" />
-						<col class="w-20" />
-					</colgroup>
-					<tr>
-						<th class="text-center">Input</th>
-						<th class="text-center">Value</th>
-					</tr>
-					{#each inputs as input, i}
-						<tr>
-							<td class="text-center">x<sub>{i + 1}</sub></td>
-							<td>
-								<BooleanButton
-									onClick={() => {
-										stop();
-										input = !input;
-									}}
-									bind:value={input}
-								/>
-							</td>
-						</tr>
-					{/each}
-				</table>
-
-				<table class="m-2 inline-block table-fixed">
-					<colgroup>
-						<col class="w-20" />
-						<col class="w-20" />
-					</colgroup>
-
-					<tr>
-						<th class="text-center">Output</th>
-						<th class="text-center">Value</th>
-					</tr>
-					{#each outputs as output, i}
-						<tr>
-							<td class="text-center">y<sub>{i + 1}</sub></td>
-							<td>
-								<BooleanTag value={output} />
-							</td>
-						</tr>
-					{/each}
-				</table>
-				<AnimationButton {next} bind:stop />
-			</div>
-		</div>
-	</FullWidth>
+	<Circuit slot="content" bind:vertices bind:edges bind:inputs />
 
 	<svelte:fragment slot="caption">
 		A circuit with 6 internal gates over the basis &#123;∧, ∨, ¬&#125; computing the Th<sub>2</sub> function:
