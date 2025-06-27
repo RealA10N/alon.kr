50b340978db23d200d5faadca2a460309b950a90
Alon Krymgand <me@alon.kr>
2025-06-27 15:11:59 +0300

    `<EyeCatcher />` so slightly less eye catching now

--- src/routes/posts/circuit-complexity/lib/EyeCatcher.svelte
+++ src/routes/posts/circuit-complexity/lib/EyeCatcher.svelte
@@ -1,6 +1,4 @@
 <div class="m-6 text-xl text-center italic">
 	At its heart, the main question in circuit complexity is
-	<div class="font-bold itali decoration-2 text-2xl text-zinc-700 dark:text-zinc-300">
-		How simple can a computation be?
-	</div>
+	<div class="font-bold itali decoration-2 text-2xl">How simple can a computation be?</div>
 </div>
