92b8f6c68132128bc06fda4632163676e314db99
Alon Krymgand <me@alon.kr>
2025-07-12 14:58:28 +0300

    WIP of `<CnfCircuit />`

--- src/routes/posts/circuit-complexity/lib/CnfCircuit.svelte
+++ src/routes/posts/circuit-complexity/lib/CnfCircuit.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import TruthTable from '$lib/logic/TruthTable.svelte';
-	import Graph, { type Edge, type Vertex } from '$lib/graphs/Graph.svelte';
+	import { type Edge, type Vertex } from '$lib/graphs/Graph.svelte';
+	import Graph from '$lib/graphs/DirectedAcyclicGraph.svelte';
 	import Figure from '$lib/Figure.svelte';
 	import AnimationButton from '$lib/AnimationButton.svelte';
 	import { bitsToId, idToBits, indexToInputs } from '$lib/logic/booleanGates';
 	import { Color } from '$lib/interfaces/color';
 	import { toSubscript } from '$lib/strings/subscripts';
 
-	let bits = [true, true, true, false];
-	const k = bits.length;
-	const n = Math.ceil(Math.log2(k));
-	const p = Math.pow(2, k);
+	let bits = [false, false, false, false];
+	$: k = bits.length;
+	$: n = Math.ceil(Math.log2(k));
 
 	const next = () => {
+		const k = bits.length;
+		const p = Math.pow(2, bits.length);
 		bits = idToBits((bitsToId(bits) + 1) % p, k);
 	};
 
@@ -61,39 +63,86 @@
 		};
 	};
 
-	const inputs = Array.from(
-		{ length: n },
-		(_, i) => ({ label: `x${toSubscript(i + 1)}` } as Vertex)
-	);
-	const negations = Array.from(
-		{ length: n },
-		(_) => ({ label: '¬', color: Color.Yellow } as Vertex)
-	);
+	const isFalseFunction = (bits: boolean[]) => bits.every((b) => !b);
 
-	const negationEdges = inputs.map((v, i) => ({
-		source: v,
-		target: negations[i],
-		direction: true
-	})) as Edge[];
+	const createInputVertex = (i: number) => ({ label: `x${toSubscript(i + 1)}` } as Vertex);
 
-	const createAndClause = (row: number): { vertices: Vertex[]; edges: Edge[] } => {
+	const createNegationVertex = (v: Vertex): { v: Vertex; e: Edge } => {
+		const neg = { label: '¬', color: Color.Yellow } as Vertex;
+		const edge = { source: v, target: neg, direction: true } as Edge;
+		return { v: neg, e: edge };
+	};
+
+	const createTargetVertex = (): Vertex => ({ label: 'y₁' } as Vertex);
+
+	const createAndVertex = () => ({ label: '∧', color: Color.Green } as Vertex);
+
+	const createFalseFunction = (): { vertices: Vertex[]; edges: Edge[] } => {
+		const input = createInputVertex(0);
+		const { v: neg, e: negEdge } = createNegationVertex(input);
+		const target = createTargetVertex();
+		const and = createAndVertex();
+
+		const inputToAnd = { source: input, target: and, direction: true } as Edge;
+		const negToAnd = { source: neg, target: and, direction: true } as Edge;
+		const andToTarget = { source: and, target: target, direction: true } as Edge;
+
+		return {
+			vertices: [input, neg, target, and],
+			edges: [negEdge, inputToAnd, negToAnd, andToTarget]
+		};
+	};
+
+	const createCnfGraph = (bits: boolean[]): { vertices: Vertex[]; edges: Edge[] } => {
+		if (isFalseFunction(bits)) return createFalseFunction();
+
+		const inputs = Array.from({ length: n }, (_, i) => createInputVertex(i));
+		const negations = inputs.map((v) => createNegationVertex(v));
+		const negationVertices = negations.map((n) => n.v);
+		const negationEdges = negations.map((n) => n.e);
+
+		let { vertices: clauseVertices, edges: clauseEdges } = createAllClauses(
+			bits,
+			inputs,
+			negationVertices
+		);
+
+		return {
+			vertices: [...inputs, ...negationVertices, ...clauseVertices],
+			edges: [...negationEdges, ...clauseEdges]
+		};
+	};
+
+	const createAndClause = (
+		row: number,
+		inputs: Vertex[],
+		negations: Vertex[]
+	): { vertices: Vertex[]; edges: Edge[] } => {
 		const inputValues = indexToInputs(row, n);
 		const inputVertices = Array.from({ length: n }, (_, i) =>
 			inputValues[i] ? inputs[i] : negations[i]
 		);
 
-		return createTree(inputVertices, { label: '∨', color: Color.Green });
+		return createTree(inputVertices, createAndVertex());
 	};
 
-	const createAllClauses = (): { vertices: Vertex[]; edges: Edge[] } => {
+	const createAllClauses = (
+		bits: boolean[],
+		inputs: Vertex[],
+		negations: Vertex[]
+	): { vertices: Vertex[]; edges: Edge[] } => {
 		const vertices = [] as Vertex[];
 		const edges = [] as Edge[];
 
 		const orSources = [] as Vertex[];
 
-		for (let i = 0; i < k; i++) {
+		for (let i = 0; i < bits.length; i++) {
 			if (!bits[i]) continue; // Skip clauses for which the bit is true
-			const { vertices: clauseVertices, edges: clauseEdges } = createAndClause(i);
+			const { vertices: clauseVertices, edges: clauseEdges } = createAndClause(
+				i,
+				inputs,
+				negations
+			);
 			vertices.push(...clauseVertices);
 			edges.push(...clauseEdges);
 
@@ -107,28 +156,28 @@
 		vertices.push(...orVertices);
 		edges.push(...orEdges);
 
-		const target = { label: 'y₁' } as Vertex;
+		const target = createTargetVertex();
 		vertices.push(target);
 		edges.push({ source: orVertices[orVertices.length - 1], target, direction: true });
 
 		return { vertices, edges };
 	};
 
-	let { vertices: clauseVertices, edges: clauseEdges } = createAllClauses();
-
-	$: vertices = [...inputs, ...negations, ...clauseVertices];
-	$: edges = [...negationEdges, ...clauseEdges];
+	let vertices = [] as Vertex[];
+	let edges = [] as Edge[];
+	let refresh = () => {};
+	$: ({ vertices, edges } = createCnfGraph(bits)), refresh();
 </script>
 
 <Figure>
 	<div slot="content" class="flex flex-row justify-center items-center">
-		<Graph {vertices} {edges} />
+		<Graph {vertices} {edges} bind:refresh />
 		<div>
 			<TruthTable title="y₁" {select} {bits} />
 			<AnimationButton bind:stop {next} />
 		</div>
 	</div>
-	<svelte:fragment slot="caption"
-		>CNF circuits over the basis &#123;∧, ∨, ¬&#125; computing computing all functions in B₂.
+	<svelte:fragment slot="caption">
+		CNF circuits over the basis &#123;∧, ∨, ¬&#125; computing computing all functions in B₂
 	</svelte:fragment>
 </Figure>
