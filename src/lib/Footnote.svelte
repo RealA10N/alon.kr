d8850f38c2ab6b4253e81439ffdae58ca5d8cf08
Alon Krymgand <me@alon.kr>
2025-07-12 10:56:03 +0300

    Updated `<Footnote />` to align last line to center

--- src/lib/Footnote.svelte
+++ src/lib/Footnote.svelte
@@ -5,13 +5,19 @@
 	const toggle = () => (focus = !focus);
 </script>
 
-<button on:click={toggle} class:focus class="rounded-full" title="Show footnote">
+<button
+	on:click={toggle}
+	class:focus
+	class="rounded-full border-zinc-600 dark:border-zinc-400"
+	title="Show footnote"
+>
 	<div class="-translate-y-1">...</div>
 </button>
 
 {#if focus}
 	<hr class="" transition:slide />
-	<div class="italic text-sm" transition:slide>
+	<div class="italic text-sm" style="text-align-last: center;" transition:slide>
 		<slot />
 	</div>
+	<hr class="" transition:slide />
 {/if}
